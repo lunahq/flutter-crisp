0126b8361da7e69a14f58d4d3342923ca676d89f
Amir Jabbari <am.jabbarii@gmail.com>
2024-01-31 12:12:40 +0330

    fix: recreate example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,52 +1,73 @@
 import 'package:crisp/crisp.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  CrispMain crispMain;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CrispMain crispMain;
 
   @override
   void initState() {
     super.initState();
 
     crispMain = CrispMain(
-      websiteId: 'WEBSITE_ID',
-      locale: 'pt-br',
+      websiteId: 'f41e1e5f-3631-4dce-9797-3e875eade4d9',
     );
 
     crispMain.register(
       user: CrispUser(
-        email: "leo@provider.com",
-        avatar: 'https://avatars2.githubusercontent.com/u/16270189?s=200&v=4',
-        nickname: "João Cardoso",
-        phone: "5511987654321",
+        email: "Amir@provider.com",
+        nickname: "Amir Jabbari",
       ),
     );
 
-    crispMain.setMessage("Hello world");
-
-    crispMain.setSessionData({
-      "order_id": "111",
-      "app_version": "0.1.1",
-    });
+    crispMain.setMessage("Amir");
+    // crispMain.appendScript("console.log(\$crisp.get('session:identifier'))");
+    // crispMain.appendScript("console.log(\$crisp.get())");
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Your brand'),
-        ),
-        body: CrispView(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: CrispView(
           crispMain: crispMain,
-          clearCache: false,
+          clearCache: true,
+          // onSessionIdReceived: (sessionId) {
+          //   print('------------- sessionIdCrisp  --------------');
+          //   print(sessionId);
+          // },
         ),
       ),
     );
